4375ad3d24719bc7e11cb44986399babc7ebc9cd
Vlad <VladDaImpaler@gmail.com>
2021-03-10 20:45:25 -0500

    added actions to reducer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,43 @@ import React, {useState} from 'react';
 import './App.css';
 import { ReactSearchAutocomplete } from 'react-search-autocomplete'
 function App() {
+  const [ingredient,setIngredient] = useState([ //temporary load from history
+    { id: 0, name: 'Beef'},
+    { id: 1, name: 'Broccoli'},
+    { id: 2, name: 'Chicken' },
+    { id: 3, name: 'Lamb' },
+    { id: 4, name: 'Tuna' }
+  ])
   
-  
+  const handleOnSearch = (string, results) => {
+    // onSearch will have as the first callback parameter
+    // the string searched and for the second the results.
+    console.log(string, results)
+  }
+
+  const handleOnSelect = (item) => {
+    // the item selected
+    console.log(item)
+  }
+
+  const handleOnFocus = () => {
+    console.log('Focused')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Entrypoint!</h1>
+      <div style={{ width: 400 }}>
+          <ReactSearchAutocomplete
+          items={ingredient}
+          onSearch={handleOnSearch}
+          onSelect={handleOnSelect}
+          onFocus={handleOnFocus}
+          autoFocus
+          />
+      </div>
+
+
       </header>
     </div>
   );
--- src/reducers/RecipeHistoryReducer.js
+++ src/reducers/RecipeHistoryReducer.js
@@ -1,8 +1,25 @@
-function RecipeReducer(state = { item: [] }, action) {
+function RecipeReducer(state = { ingredient: '', loading: false }, action) {
     switch (action.type) {
-
-
-
+        case 'LOADING_HISTORY':
+            return {
+                ...state,
+                ingredient: [...state.ingredient],
+                loading: true
+            }
+        
+        case 'ADD_RECIPE':
+            return {
+                ...state,
+                ingredient = state.ingredient.concat(action.ingredient),
+                loading: false
+            }
+        
+        case 'CLEAR_HISTORY':
+            return {
+                ...state,
+                ingredient: '',
+                loading: false
+            }
         default:
             return state;
     }
