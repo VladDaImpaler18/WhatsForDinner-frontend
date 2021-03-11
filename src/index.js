9838078f1b98ce410cc15ff3c0a5a7f741194835
Vlad <VladDaImpaler@gmail.com>
2021-03-11 00:19:22 -0500

    added composeWithDevTools

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { Provider } from 'react-redux';
 import RecipeReducer from './reducers/RecipeHistoryReducer.js';
 
 const store = createStore(
   RecipeReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeWithDevTools(applyMiddleware(thunk))
 );
 
 
