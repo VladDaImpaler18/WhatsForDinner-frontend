2b9f85c57915e2b5d3dde198d8e3e8e674b60dcc
Vlad <VladDaImpaler@gmail.com>
2021-03-15 14:04:23 -0400

    added handleOnSubmit

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,10 +3,15 @@ import React, {useState} from 'react'
 function SearchForm(props) {
     const [value, setValue] = useState('')
 
+    const handleOnSubmit = (e) => {
+        e.preventdefault()
+        
+
+    }
     return (
         <div>
             <lable>Primary Ingredient: </lable>
-            <input type='text' value={value} onChange={ (e)=>setValue(e.target.value) }>
+            <input type='text' value={value} onChange={ (e)=>setValue(e.target.value) } onSubmit={handleOnSubmit}>
             
             
             </input>
