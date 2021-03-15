f287a5b0310d9ceffcf499c54a44bf39a309a166
Vlad <VladDaImpaler@gmail.com>
2021-03-15 11:44:17 -0400

    reworked reducer for use with hooks and useReducer

--- src/reducers/RecipeHistoryReducer.js
+++ src/reducers/RecipeHistoryReducer.js
@@ -1,3 +1,19 @@
+const initialState = { recipes: [], loading: false};
+const RecipeReducer = (state, action) =>{
+    debugger //check exactly how action is structured
+    switch (action) {
+        case 'LOADING_HISTORY':
+            return {...state,  recipes: [...state.recipes], loading:true};
+        case 'ADD_RECIPE':
+            return {...state, recipes: state.recipes.concat(action.recipe), loading: false};
+        case 'CLEAR_HISTORY': return {...state, recipes: [], loading: false};
+        case 'SELECT_RECIPE':
+            debugger
+            return {...state, loading: false}
+        case 'DELETE_RECIPE':
+            return {...state, recipes: state.recipes.filter(r => r !== action.recipe), loading: false };
+    }
+}
 /* Legacy Way
 function RecipeReducer(state = { recipe: '', loading: false }, action) {
     switch (action.type) {
