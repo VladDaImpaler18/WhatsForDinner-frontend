7a2a1c0830f414a55e5d2dfa650cc3514dabc272
Vlad <VladDaImpaler@gmail.com>
2021-03-16 00:44:31 -0400

    added flexbox styling

---
+++ src/components/Recipe.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Recipe.css"
+
+const Recipe = props => {
+    return (
+        <div class="recipe">
+            
+        </div>
+    )
+}
+
+export default Recipe;
+
+/*
+Recipie(name, URL, image, description, story, ingredients [],
+         directions [], review, notes, createDate, updateDate)
+*/
