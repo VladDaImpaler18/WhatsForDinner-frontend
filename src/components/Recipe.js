3286392cb3290a2c8b766983ab7516e98491a5b1
Vlad <VladDaImpaler@gmail.com>
2021-03-16 01:26:26 -0400

    Presentational component, displays image and title with flexbox styling

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,9 +2,14 @@ import React from 'react'
 import "./Recipe.css"
 
 const Recipe = props => {
+    const str = props.thumb.split("/");
+    const imageName = str[str.length-1];
+    const imageLocation = "../../images/".concat(imageName);
+
     return (
-        <div class="recipe">
-            
+        <div key={props.id} class="recipe">
+            <img src={imageLocation} alt="Thumbnail" />
+            <p>{props.title}</p>
         </div>
     )
 }
@@ -14,4 +19,9 @@ export default Recipe;
 /*
 Recipie(name, URL, image, description, story, ingredients [],
          directions [], review, notes, createDate, updateDate)
+
+        "title": "",
+        "thumb": "/sbx7n71587673021.jpg",
+        "id": "6"
+      },
 */
