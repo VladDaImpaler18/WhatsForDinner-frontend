7d78b4d5f2c892f1a7de37cb175e39e49485c2bb
Vlad <VladDaImpaler@gmail.com>
2021-03-17 02:37:54 -0400

    destructured props to {recipe}

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import "./Recipe.css"
 
-const Recipe = props => {
+const Recipe = ({recipe}) => {
+    const str = recipe.thumb.split("/");
+    const imageName = str[str.length-1];
+    const imageLocation = "../../images/".concat(imageName);
+
     return (
-        <div class="recipe">
-            
+        <div key={recipe.id} class="container-recipe">
+            <img src={imageLocation} alt="Thumbnail" />
+            <p>{recipe.title}</p>
         </div>
     )
 }
@@ -14,4 +19,9 @@ export default Recipe;
 /*
 Recipie(name, URL, image, description, story, ingredients [],
          directions [], review, notes, createDate, updateDate)
+
+        "title": "",
+        "thumb": "/sbx7n71587673021.jpg",
+        "id": "6"
+      },
 */
