e47cf46df782b67dd5671ecf3838988d9d9b549d
Vlad <VladDaImpaler@gmail.com>
2021-03-17 22:29:39 -0400

    cleaned up state, removed unnecessary action.types

--- src/reducers/RecipeHistoryReducer.js
+++ src/reducers/RecipeHistoryReducer.js
@@ -1,19 +1,25 @@
-const RecipeReducer = (state = {recipes: [], loading: false}, action) =>{
+const initialState = {recipes: [], loading: false}
+const RecipeReducer = (state={ recipes: [], loading: false }, action) =>{
     //debugger //check exactly how action is structured
-    switch (action) {
-        case 'LOADING_HISTORY':
-            return {...state,  recipes: [...state.recipes], loading:true};
+    switch (action.type) {
+        case 'LOADING_DATA':
+            return {...state, ingredient: state.ingredient, recipes: state.recipes, loading: true};
+    
         case 'ADD_RECIPE':
             return {...state, recipes: state.recipes.concat(action.recipe), loading: false};
-        case 'CLEAR_HISTORY': return {...state, recipes: [], loading: false};
+
+        case 'CLEAR_ALL': return {initialState};
+
         case 'SELECT_RECIPE':
-            //debugger
+            debugger
             return {...state, loading: false}
         case 'DELETE_RECIPE':
             return {...state, recipes: state.recipes.filter(r => r !== action.recipe), loading: false };
+        
         default: return state;
     }
 }
+export default RecipeReducer
 /* Legacy Way
 function RecipeReducer(state = { recipe: '', loading: false }, action) {
     switch (action.type) {
@@ -56,4 +62,3 @@ function RecipeReducer(state = { recipe: '', loading: false }, action) {
     }
 }
 */
-export default RecipeReducer
