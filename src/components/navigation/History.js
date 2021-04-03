86d322a25b80bff3c12349c6f1d737cdc67a6e7c
Vlad <VladDaImpaler@gmail.com>
2021-04-03 14:50:06 -0400

    added UL and LI objects based on ingredients, ingredient state hooks

--- src/components/navigation/History.js
+++ src/components/navigation/History.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import jsondata from '../../.cache/recipeData'
 
 function History(){
-
+//component did mount fetch user's history from backend
+/*****local state***** after fetching
+ingredients: [ingredient1, ..., ingredientN]
+selection: null
+recipes: [] (length = 0)
+*/
+let initialState = ["chicken", "beef", "tofu", "corn", "broccoli", ]
+    const [ingredients, setingredients] = useState(initialState)
+debugger
     return(
-        <div>
-            <h1>This is the history page!</h1>
-        </div>
+        <>
+            <ul>
+                {ingredients.map(ingredient => {
+                    <li key={ingredient.id}>
+                        {ingredient}
+                    </li>
+                })}
+            </ul>
+        </>
     )
 }
 
