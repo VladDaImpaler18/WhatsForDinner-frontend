ee1fe6a27bd6156ee7730e72e955a6162663185b
Vlad <VladDaImpaler@gmail.com>
2021-04-04 00:06:17 -0400

    installed npm packages  react-window and react-virtualized-auto-sizer; set up basic list to expand on

--- src/components/navigation/History.js
+++ src/components/navigation/History.js
@@ -1,6 +1,11 @@
 import React, {useState, useEffect} from 'react'
 import jsondata from '../../.cache/recipeData'
 
+import { FixedSizeList as List } from 'react-window';
+import AutoSizer from "react-virtualized-auto-sizer";
+
+import "./History.css";
+
 function History(){
 //component did mount fetch user's history from backend
 /*****local state***** after fetching
@@ -8,18 +13,35 @@ ingredients: [ingredient1, ..., ingredientN]
 selection: null
 recipes: [] (length = 0)
 */
-let initialState = ["chicken", "beef", "tofu", "corn", "broccoli", ]
-    const [ingredients, setingredients] = useState(initialState)
-debugger
+let initialState = ["chicken", "beef", "tofu", "corn", "broccoli"]
+    const [ingredients, setIngredients] = useState(initialState)
+
+    //{data, index, style}
+    const row = ({index, style}) => {
+        //maybe show some thumbnails of included recipes.
+        return(
+        <div className={index % 2 ? "ListItemOdd" : "ListItemEven"} style={style}>
+            {ingredients[index]}
+        </div>
+    )}
+    
     return(
         <>
-            <ul>
-                {ingredients.map(ingredient => {
-                    <li key={ingredient.id}>
-                        {ingredient}
-                    </li>
-                })}
-            </ul>
+        <p>Cookbook history</p>
+            <AutoSizer>
+              {({height, width}) => (
+                <List 
+                  className="List"
+                  height={height}
+                  itemCount={ingredients.length}
+                  itemSize={35}
+                  width={width}
+                >
+                  {row}
+                </List>
+            )
+              }
+            </AutoSizer>
         </>
     )
 }
