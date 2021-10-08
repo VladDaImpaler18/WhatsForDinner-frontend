f97bb34beef5ec95f5d9c2ed06802def2f27b9ad
vlad <VladDaImpaler@gmail.com>
2021-10-08 03:45:09 -0400

    added addMeal action and required constants

--- src/actions/recipeActions.js
+++ src/actions/recipeActions.js
@@ -1,7 +1,8 @@
-import { FETCH_MEAL_REQUEST, FETCH_MEAL_SUCCESS, FETCH_MEAL_FAIL } from "../constants/action-type";
+import { IMPORT_MEAL_REQUEST, IMPORT_MEAL_SUCCESS, IMPORT_MEAL_FAIL, CREATE_MEAL_REQUEST, CREATE_MEAL_SUCCESS, CREATE_MEAL_FAIL } from "../constants/action-type";
   const baseURL = "http://localhost:3000"
 
   const importMeal = (url) => { 
+    debugger;
     return(dispatch) => {
       let configObj = {
         method: "POST",
@@ -12,13 +13,13 @@ import { FETCH_MEAL_REQUEST, FETCH_MEAL_SUCCESS, FETCH_MEAL_FAIL } from "../cons
         body: JSON.stringify(url)
 
       };
-    dispatch({ type: FETCH_MEAL_REQUEST });
+    dispatch({ type: IMPORT_MEAL_REQUEST });
     fetch(`${baseURL}/meals/import`, configObj)
          .then(response => response.json())
          .then(mealData => {
-             dispatch({ type: FETCH_MEAL_SUCCESS, payload: mealData })
+             dispatch({ type: IMPORT_MEAL_SUCCESS, payload: mealData })
           })
-         .catch(err => dispatch({ type: FETCH_MEAL_FAIL, payload: err })); 
+         .catch(err => dispatch({ type: IMPORT_MEAL_FAIL, payload: err })); 
     };
   }
 
@@ -32,14 +33,18 @@ import { FETCH_MEAL_REQUEST, FETCH_MEAL_SUCCESS, FETCH_MEAL_FAIL } from "../cons
         },
         body: JSON.stringify(meal)
       };
-      fetch(`${baseURL}`)
+      dispatch( { type: CREATE_MEAL_REQUEST });
+      fetch(`${baseURL}/meals`, configObj)
+        .then(response => response.json())
+        .then(mealData => {
+          dispatch({ type: CREATE_MEAL_SUCCESS, payload: mealData })
+        })
+        .catch(err => dispatch({ type: CREATE_MEAL_FAIL, payload: err }));
     };
   }
 
   //selectMeal
   
-
-  //addMeal
 
   //deleteMeal
 
