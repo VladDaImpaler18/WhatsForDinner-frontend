296f2d44e2d18f30ce3e166b02331a8fc8c9b663
vlad <VladDaImpaler@gmail.com>
2021-10-29 01:04:59 -0400

    not dispatching correctly

--- src/actions/mealActions.js
+++ src/actions/mealActions.js
@@ -1,7 +1,7 @@
 import { IMPORT_MEAL_REQUEST, IMPORT_MEAL_SUCCESS, IMPORT_MEAL_FAIL, CREATE_MEAL_REQUEST, CREATE_MEAL_SUCCESS, CREATE_MEAL_FAIL } from "../constants/action-type";
   const baseURL = "http://127.0.0.1:3001"
 
-  const importMeal = (url) => { 
+  export const importMeal = (url) => { 
     debugger;
     return(dispatch) => {
       let configObj = {
@@ -17,13 +17,14 @@ import { IMPORT_MEAL_REQUEST, IMPORT_MEAL_SUCCESS, IMPORT_MEAL_FAIL, CREATE_MEAL
     fetch(`${baseURL}/meals/import`, configObj)
          .then(response => response.json())
          .then(mealData => {
-             dispatch({ type: IMPORT_MEAL_SUCCESS, payload: mealData })
+             return dispatch({ type: IMPORT_MEAL_SUCCESS, payload: mealData })
           })
          .catch(err => dispatch({ type: IMPORT_MEAL_FAIL, payload: err })); 
     };
   }
 
-  export const addMeal = (meal) => (dispatch) => {
+  export const addMeal = (meal) => {
+    (dispatch) => {
       let configObj = {
         method: "POST",
         headers: {
@@ -42,6 +43,7 @@ import { IMPORT_MEAL_REQUEST, IMPORT_MEAL_SUCCESS, IMPORT_MEAL_FAIL, CREATE_MEAL
         })
         .catch(err => dispatch({ type: CREATE_MEAL_FAIL, payload: err }));
     };
+  }
   
 
   //selectMeal
