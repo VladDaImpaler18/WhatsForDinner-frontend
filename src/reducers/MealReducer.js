3bf8163f30b315fd4f6305a65f88a1d7304a28de
vlad <VladDaImpaler@gmail.com>
2021-10-30 02:14:39 -0400

    set up error payloads and import meal

--- src/reducers/MealReducer.js
+++ src/reducers/MealReducer.js
@@ -1,19 +1,18 @@
 import { IMPORT_MEAL_REQUEST, IMPORT_MEAL_SUCCESS, IMPORT_MEAL_FAIL, CREATE_MEAL_REQUEST, CREATE_MEAL_SUCCESS, CREATE_MEAL_FAIL } from "../constants/action-type";
 let meal = {title: '',  category: '', ingredients:[], instructions: [], tags: [], source: ''}
-
-const MealReducer = (state={ meal, loading: false, err:'' }, action) =>{
+let err = ''
+const MealReducer = (state={ meal, loading: false, err }, action) =>{
     switch (action.type) {
         case IMPORT_MEAL_REQUEST:
             return {...state, loading: true}
             
         case IMPORT_MEAL_SUCCESS:
             meal = action.payload
-            debugger
             return {...state, meal, loading: false}
         
         case IMPORT_MEAL_FAIL:
             err = action.payload
-            return {...state, loading: false}
+            return {...state, err, loading: false}
 
         case CREATE_MEAL_REQUEST:
             return {...state, loading: true}
