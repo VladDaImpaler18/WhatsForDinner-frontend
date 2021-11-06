2e8b6e11c78ea18f1db50d8eb059fd1a6aa927f3
vlad <VladDaImpaler@gmail.com>
2021-11-06 00:08:30 -0400

    clean up comments and unused resources

--- src/containers/NewMeal.js
+++ src/containers/NewMeal.js
@@ -1,33 +1,22 @@
 //meal container
-import React, {useEffect, useState} from 'react'
 import ImportForm from '../components/ImportForm'
 import MealForm from '../components/MealForm'
 import {useSelector, useDispatch } from 'react-redux'
 import { addMeal, importMeal } from '../actions/mealActions'
 
 function NewMeal() {
-    // Meal states to be passed
-    let initialState = {
-        title: '',
-        category: '',
-        ingredients: ['','',''],
-        instructions: ['','',''],
-        tags:[''],
-        source:''
-        }
-    const [meal, setMeal] = useState(initialState)
-    
+
+    const meal = useSelector(state => state.meal)
     const dispatch = useDispatch();
+
+    
  
     const handleSubmit = (meal) => {      
-        //do addMeal stuff
         dispatch(addMeal(meal))
     }
 
     const handleImport = (meal) => {
-        //do import stuff
         dispatch(importMeal(meal))
-
     }
 
 
